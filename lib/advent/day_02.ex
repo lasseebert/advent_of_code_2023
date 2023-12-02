2d5977b0d55b024a4db4336ea8e9b269fb78e61a
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2023-12-02 19:47:14 +0100

    D2p1

---
+++ lib/advent/day_02.ex
@@ -0,0 +1,82 @@
+defmodule Advent.Day02 do
+  @moduledoc """
+  Day 02
+  """
+
+  @doc """
+  Part 1
+  """
+  @spec part_1(String.t()) :: integer
+  def part_1(input) do
+    games = input |> parse()
+
+    max = %{
+      red: 12,
+      green: 13,
+      blue: 14
+    }
+
+    games
+    |> Enum.filter(fn {_id, game} ->
+      for(round <- game, {color, max_amount} <- max, do: {round, color, max_amount})
+      |> Enum.all?(fn {round, color, max_amount} -> Map.get(round, color, 0) <= max_amount end)
+    end)
+    |> Enum.map(fn {id, _game} -> id end)
+    |> Enum.sum()
+  end
+
+  @doc """
+  Part 2
+  """
+  @spec part_2(String.t()) :: integer
+  def part_2(input) do
+    input
+    |> parse()
+
+    0
+  end
+
+  defp parse(input) do
+    input
+    |> String.trim()
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
+  end
+
+  defp parse_line(line) do
+    [id_text, game_text] = line |> String.split(": ")
+    {parse_id(id_text), parse_game(game_text)}
+  end
+
+  defp parse_id("Game " <> id_string), do: String.to_integer(id_string)
+
+  defp parse_game(game_text) do
+    game_text
+    |> String.split("; ")
+    |> Enum.map(&parse_round/1)
+  end
+
+  defp parse_round(round_text) do
+    round_text
+    |> String.split(", ")
+    |> Enum.map(&parse_color/1)
+    |> Map.new()
+  end
+
+  defp parse_color(color_text) do
+    color_text
+    |> String.split(" ")
+    |> then(fn [amount, color] ->
+      amount = String.to_integer(amount)
+
+      color =
+        case color do
+          "red" -> :red
+          "blue" -> :blue
+          "green" -> :green
+        end
+
+      {color, amount}
+    end)
+  end
+end
---
+++ test/advent/day_02_test.exs
@@ -0,0 +1,39 @@
+defmodule Advent.Day02Test do
+  use Advent.Test.Case
+
+  alias Advent.Day02
+
+  @example_input """
+  Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+  Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+  Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+  Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+  Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+  """
+
+  @puzzle_input File.read!("puzzle_inputs/day_02.txt")
+
+  describe "part 1" do
+    test "example" do
+      assert Day02.part_1(@example_input) == 8
+    end
+
+    @tag :puzzle_input
+    test "puzzle input" do
+      assert Day02.part_1(@puzzle_input) == 2685
+    end
+  end
+
+  describe "part 2" do
+    @tag :skip
+    test "example" do
+      assert Day02.part_2(@example_input) == :foo
+    end
+
+    @tag :skip
+    @tag :puzzle_input
+    test "puzzle input" do
+      assert Day02.part_2(@puzzle_input) == :foo
+    end
+  end
+end
