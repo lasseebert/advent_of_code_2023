728214bc66d82ee1a4f69fd2edaf612d961efdd6
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2023-12-21 22:24:57 +0100

    D21P2

--- lib/advent/day_21.ex
+++ lib/advent/day_21.ex
@@ -6,55 +6,143 @@ defmodule Advent.Day21 do
   @doc """
   Part 1
   """
-  @spec part_1(String.t()) :: integer
+  @spec part_1(String.t(), pos_integer) :: integer
   def part_1(input, steps \\ 64) do
-    {map, start} = input |> parse()
-
-    rem_2 = rem(steps, 2)
-
-    [start]
-    |> walk([], 0, %{}, map, steps)
-    |> Map.values()
-    |> Enum.count(&(rem(&1, 2) == rem_2))
+    solve(input, steps)
   end
 
-  defp walk(_, _, distance, visited, _, steps) when distance > steps, do: visited
-  defp walk([], [], _distance, visited, _map, _steps), do: visited
+  @doc """
+  Part 2
+  """
+  @spec part_2(String.t(), pos_integer) :: integer
+  def part_2(input, steps \\ 26_501_365) do
+    solve(input, steps)
+  end
 
-  defp walk([coord | coords], next_coords, distance, visited, map, steps) do
+  defp solve(input, steps) do
+    {map, start} = input |> parse()
+
+    size_x = map |> Enum.map(fn {x, _} -> x end) |> Enum.max() |> Kernel.+(1)
+    size_y = map |> Enum.map(fn {_, y} -> y end) |> Enum.max() |> Kernel.+(1)
+    dims = {size_x, size_y}
+
+    # Since there are open spaces all around the edges of the map, we can eventually
+    # get from one map to the next (in infinite mode) in the same number of steps as
+    # the size of the map. This will lead to a cycle of quadratic numbers.
+    true = size_x == size_y
+    cycle_length = size_x
+
+    # We only need to count only the odd or event distances, depending on whether the
+    # steps is odd or even.
+    rem_2 = rem(steps, 2)
+
+    # A stream that yields the number of tiles we can visit and the distance so far.
+    # Each result from the stream is two steps (odd or even)
+    layer_stream =
+      Stream.unfold({[start], 0, %{}, 0}, fn {coords, distance, visited, total} ->
+        total =
+          if rem(distance, 2) == rem_2 do
+            total + length(coords)
+          else
+            total
+          end
+
+        {next_coords, next_visited} = walk_one_layer(coords, [], distance, visited, map, dims)
+        {{total, distance}, {next_coords, distance + 1, next_visited, total}}
+      end)
+      |> Stream.drop(rem_2)
+      |> Stream.take_every(2)
+
+    # Since we move two steps at a time in the stream, we multiply the cycle length by 2.
+    # This is the number of steps we need to take first for a full number of cycles to be left.
+    steps_first_cycle = rem(steps, cycle_length * 2)
+
+    if steps_first_cycle == steps do
+      # Special case: We move so few steps we don't need to worry about cycles
+      layer_stream
+      |> Enum.at(div(steps, 2))
+      |> elem(0)
+    else
+      layer_stream
+      |> Stream.drop(div(steps_first_cycle, 2))
+      |> Stream.take_every(cycle_length)
+      |> Stream.chunk_every(4, 1, :discard)
+      |> Stream.map(fn [{a, _}, {b, _}, {c, _}, {d, dist}] -> {{a, b, c, d}, dist} end)
+      # {a, b, c, d} are four consecutive numbers of visited tiles.
+      # We want to find a "quadratic" cycle.
+      #
+      # For a linear cycle, we sould look for a constant linear increase like this.
+      # Diffs between numbers are written below. When we have a zero diff, we have a cycle.
+      #
+      #   3   4   6   8   10
+      #     1   2   2   2
+      #       1   0   0
+      #
+      # For quadratic cycles, we look for the same thing, but we need to go one level further down,
+      # which is why we need four numbers.
+      #
+      # To get the sum we want to be zero, we calculate second layer:
+      #
+      #   b - a, c - b, d - c
+      #
+      # Then third layer:
+      #
+      #   (c - b) - (b - a), (d - c) - (c - b)
+      #
+      # Then the last layer:
+      #
+      #   ((d - c) - (c - b)) - ((c - b) - (b - a))
+      #   = d - 3c + 3b - a
+      |> Enum.find(fn {{a, b, c, d}, _dist} -> d - 3 * c + 3 * b - a == 0 end)
+      |> then(fn {{_a, b, c, d}, dist} ->
+        # Now that we have the numbers, we need to first figure out how many full cycles we have
+        # left:
+        x = div(steps - dist, cycle_length * 2)
+
+        # Then we calculate the first layer value at that many iterations forward:
+        #
+        # * Layer 1 is not repeated (we only add to it)
+        # * Layer 2 is repeated exactly x times
+        # * Layer 3 is repeated exactly x * (x + 1) / 2 times (sum of 1..x)
+        l1 = d
+        l2 = d - c
+        l3 = d - 2 * c + b
+
+        l1 + l2 * x + div(l3 * x * (x + 1), 2)
+      end)
+    end
+  end
+
+  defp walk_one_layer([], next_coords, _distance, visited, _map, _dims) do
+    {Enum.uniq(next_coords), visited}
+  end
+
+  defp walk_one_layer([coord | coords], next_coords, distance, visited, map, dims) do
     visited = Map.put(visited, coord, distance)
 
     next_coords =
       coord
-      |> neighbours(map, visited)
+      |> neighbours(map, visited, dims)
       |> Enum.reduce(next_coords, &[&1 | &2])
 
-    walk(coords, next_coords, distance, visited, map, steps)
+    walk_one_layer(coords, next_coords, distance, visited, map, dims)
   end
 
-  defp walk([], next_coords, distance, visited, map, steps) do
-    walk(Enum.uniq(next_coords), [], distance + 1, visited, map, steps)
-  end
-
-  defp neighbours({x, y}, map, vistied) do
+  defp neighbours({x, y}, map, visited, dims) do
     [
       {x - 1, y},
       {x + 1, y},
       {x, y - 1},
       {x, y + 1}
     ]
-    |> Enum.filter(fn coord -> MapSet.member?(map, coord) and not Map.has_key?(vistied, coord) end)
+    |> Enum.filter(fn coord -> on_map?(coord, map, dims) and not Map.has_key?(visited, coord) end)
   end
 
-  @doc """
-  Part 2
-  """
-  @spec part_2(String.t()) :: integer
-  def part_2(input) do
-    input
-    |> parse()
+  defp on_map?({x, y}, map, {size_x, size_y}) do
+    map_x = rem(rem(x, size_x) + size_x, size_x)
+    map_y = rem(rem(y, size_y) + size_y, size_y)
 
-    0
+    MapSet.member?(map, {map_x, map_y})
   end
 
   defp parse(input) do
@@ -77,16 +165,17 @@ defmodule Advent.Day21 do
       end)
       |> Enum.into(%{})
 
+    [start] =
+      map
+      |> Enum.filter(fn {_, type} -> type == :start end)
+      |> Enum.map(fn {coord, _} -> coord end)
+
     open =
       map
       |> Enum.filter(fn {_, type} -> type == :open end)
       |> Enum.map(fn {coord, _} -> coord end)
       |> MapSet.new()
-
-    [start] =
-      map
-      |> Enum.filter(fn {_, type} -> type == :start end)
-      |> Enum.map(fn {coord, _} -> coord end)
+      |> MapSet.put(start)
 
     {open, start}
   end
--- test/advent/day_21_test.exs
+++ test/advent/day_21_test.exs
@@ -31,15 +31,37 @@ defmodule Advent.Day21Test do
   end
 
   describe "part 2" do
-    @tag :skip
-    test "example" do
-      assert Day21.part_2(@example_input) == :foo
+    test "example 1" do
+      assert Day21.part_2(@example_input, 6) == 16
     end
 
-    @tag :skip
+    test "example 2" do
+      assert Day21.part_2(@example_input, 10) == 50
+    end
+
+    test "example 3" do
+      assert Day21.part_2(@example_input, 50) == 1594
+    end
+
+    test "example 4" do
+      assert Day21.part_2(@example_input, 100) == 6536
+    end
+
+    test "example 5" do
+      assert Day21.part_2(@example_input, 500) == 167_004
+    end
+
+    test "example 6" do
+      assert Day21.part_2(@example_input, 1000) == 668_697
+    end
+
+    test "example 7" do
+      assert Day21.part_2(@example_input, 5000) == 16_733_044
+    end
+
     @tag :puzzle_input
     test "puzzle input" do
-      assert Day21.part_2(@puzzle_input) == :foo
+      assert Day21.part_2(@puzzle_input) == 602_259_568_764_234
     end
   end
 end
